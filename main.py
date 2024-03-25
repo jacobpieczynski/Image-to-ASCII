3380f73ba13f11b3ffafbdd128bf908239399dfe
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-03-25 14:32:29 -0700

    misc testing/edits

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 ## Image to ASCII Art
-
-import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from sys import argv
 
 ascii = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+RESIZE_FACTOR = 2
 
 def main():
     # Gets the filename, either with a default or user-inputted value
@@ -17,6 +16,8 @@ def main():
     try:
         image = Image.open(filename)
         print('Successfully loaded image')
+        width, height = image.size[0], image.size[1]
+        image = image.resize((width // RESIZE_FACTOR, height // RESIZE_FACTOR))
         print(f'Image size: {image.size[0]} x {image.size[1]} pixels')
     except:
         print('Failed to load image')
@@ -25,7 +26,6 @@ def main():
     # Convert the image to an array
     print('Converting image to array...')
     img_array = np.array(image)
-    print(img_array.shape)
     
     ascii_image = []
 
@@ -35,20 +35,27 @@ def main():
         ascii_line = []
         for pixel in line:
             r, g, b = int(pixel[0]), int(pixel[1]), int(pixel[2])
+
             # 3 Methods of determining brightness: Average, Luminosity, and Lightness
             brightness = (r + g + b) / 3 # Average
             #brightness = 0.21*r + 0.72*g + 0.07*b # Luminosity
-            #brightness = max(r, g, b) + min(r, g, b) / 2 # Lightness
+            #brightness = min(round(max(r, g, b) + min(r, g, b) / 2), 254) # Lightness
 
             char = ascii[int(brightness / 255 * len(ascii))]
+
+            # 3 characters per pixel (to adjust for most characters being 3x as tall as they are wide)
+            ascii_line.append(char)
+            ascii_line.append(char)
             ascii_line.append(char)
         ascii_image.append(ascii_line)
 
     # Print the ASCII image
-    num_array = np.array([[ord(char) for char in row] for row in ascii_image])
-    plt.imshow(num_array, cmap='gray', vmin=0, vmax=255, interpolation='none')
-    plt.axis('off')  # Turn off axis labels
-    plt.show()
+    print('Printing ASCII image...')
+    with open('ascii_image_average.txt', 'w') as f:
+        for line in ascii_image:
+            for pixel in line:
+                f.write(pixel)
+            f.write('\n')
 
 if __name__ == '__main__':
     main()
