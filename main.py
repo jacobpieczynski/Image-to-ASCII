c437793d99271ba219c1aec488c859e6177201e1
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-03-25 13:46:37 -0700

    read image

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 ## Image to ASCII Art
+from PIL import Image
 from sys import argv
 
 def main():
-    height, width = 0, 0
-    filename = 'test.jpg'
-    if len(argv) > 1:
-        filename = argv[1]
-    print(f'Filename: {filename}')
+    # Gets the filename, either with a default or user-inputted value
+    filename = 'images/test.jpg'
+    if len(argv) > 1: filename = argv[1]
+    print(f'File: {filename}')
 
     # Read the image
-    if load_image(filename):
+    try:
+        image = Image.open(filename)
         print('Successfully loaded image')
-
-
-def load_image(filename):
-    print('Loading image...')
+        print(f'Image size: {image.size[0]} x {image.size[1]} pixels')
+    except:
+        print('Failed to load image')
+        return
 
 
 if __name__ == '__main__':
