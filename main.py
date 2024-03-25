6a1f67bf4734c24db27692bf8576213935f0ac59
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-03-25 13:39:42 -0700

    structure to load image

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 ## Image to ASCII Art
+from sys import argv
 
 def main():
-    print('Hello World')
+    height, width = 0, 0
+    filename = 'test.jpg'
+    if len(argv) > 1:
+        filename = argv[1]
+    print(f'Filename: {filename}')
+
+    # Read the image
+    if load_image(filename):
+        print('Successfully loaded image')
+
+
+def load_image(filename):
+    print('Loading image...')
 
 
 if __name__ == '__main__':
